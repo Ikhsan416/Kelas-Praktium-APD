9ea5c3c7246b32780b60cb6bb21edf3cd2df1ad5
2409106118_Ikhsan <114375653+Ikhsan416@users.noreply.github.com>
2024-09-18 17:15:02 +0800

    Test

---
+++ Kelas-Praktikum-APD/Pertemuan-3/2409106118_Ikhsan_C2'24.py
@@ -0,0 +1,25 @@
+# cuaca = "Mendung"
+
+# if cuaca == "mendung":
+#     print("Main Ml Dirumah Aja")
+# else:
+#     print("Mendung Doang Gas Aja Jalan")
+
+# umur = int(print("Masukkan Umurmu: "))
+# if umur > 0:
+#     if umur <= 10:
+#         print("Umur Anda Masih Kecil")
+#     elif umur <= 18:
+#         print("Umur Anda Sudah Remaja")
+#     elif umur <= 35:
+#         print("Umur Anda Sudah Mulai Menua")
+#     elif umur <= 65:
+#         print("Waduh Semoga Kamu Sehat Sehat Ya")
+#     else:
+#         print("apakamu masih hidup")
+# else:
+#     print("Bisa Ngetik Angka Ga Dek!!!!")
+        
+# nim =  int(input("Masukkan Nim; "))
+# hasil = "Kelas A" if nim >= 1 and nim <= 50 else "Kelas B" if nim >= 51 and nim <= 100 else "Kelas C" if nim >= 101 and nim <= 123 else "KAMU ORANG DALAM"
+# print(hasil)
