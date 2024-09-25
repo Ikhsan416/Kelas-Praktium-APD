b9b0730b5fe51c97d439feda8d124aa4ec9aa59e
2409106118_Ikhsan <114375653+Ikhsan416@users.noreply.github.com>
2024-09-25 17:31:52 +0800

    Pertemuan4(Pengulangan)

---
+++ Kelas-Praktikum-APD/Pertemuan-4/2409106118_Ikhsan)_C2'24.py
@@ -0,0 +1,45 @@
+# ulang = 10
+# for i in range(ulang):
+#     print("Perulangan ke-" + str(i))
+
+# for i in range(2, 10):
+#     print(f"Assalamualaikum{i}")
+
+# for i in range (2, 100, 10):
+#     print(f"Ikhsan Ganteng")
+
+# for Ikhsan in range(1, 4):
+#     for Jamal in range(1, 4):
+#         print(f"{Ikhsan} x {Jamal} = {Ikhsan * Jamal}")
+#     print()
+
+# lanjut = 'isan'
+# hitung = 0
+# while(lanjut == 'isan'):
+#         hitung += 1
+#         lanjut = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+# while True:
+#     print("IkhsanGanteng")
+
+# INI BREAK
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Lanjut Ga Cuy? ")
+#     if ulang == "off" or ulang =="OFF" or ulang =="Off":
+#         break
+# print(f"Total Perulangan: {hitung}")
+
+# INI CONTINUE
+# print('Daftar bilangan ganjil dari 1-10')
+# for i in range(100):
+#     if i % 2 != 0:
+#         continue
+#     print(i)
+
+# kelasC =["Melchi", 2409106117, "Ikhsan", 2409106118,"Rafly", 2409106119, "Willy", 2409106120,"Dimas", 2409106121]
+# for i in range(len(kelasC)):
+#     print(f"{i+1}. {kelasC[i]}")
+# print(kelasC)
