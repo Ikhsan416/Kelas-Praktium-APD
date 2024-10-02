8b4bb79e753c4d5e4a623fbfbfe7aa479ba85bd7
118Ikhsan <you@example.com>
2024-10-02 16:24:40 +0800

    Update P5

---
+++ Kelas-Praktikum-APD/Pertemuan-5/2409106118_Ikhsan_C2'24.py
@@ -0,0 +1,24 @@
+# Nama Teman
+# Teman = ["Duan","Fijri","Reja","Paras"]
+# print(Teman)
+
+#berikut adalah mendefinisikan list tas
+# makanan = ["Crepes Pisang Coklat","Burger Bangor","Ayam Geprek","Nasi Padang", ["+ Nasi","+ Jus Alpuat Milo"]]
+# # print(makanan)
+# makanan.append("Kamu")
+# print(makanan)
+
+# outfit = [ 
+#             "Crewneck",
+#             ["Baju Hitam",
+#             "Jeans Biru",
+#             "Chino Hitam",
+#             "Sneakers",
+#             "Bucket Hat",]
+#                 ]
+# print(outfit)
+
+# for i in outfit :
+#     if isinstance (i, list):
+#         for j in i :
+#             print(j)
