87b2cf599da946f90a6455023089320d8ee62d09
118Ikhsan <you@example.com>
2024-10-02 16:46:30 +0800

    Pertemuan5

--- Kelas-Praktikum-APD/Pertemuan-5/2409106118_Ikhsan_C2'24.py
+++ Kelas-Praktikum-APD/Pertemuan-5/2409106118_Ikhsan_C2'24.py
@@ -22,3 +22,32 @@
 #     if isinstance (i, list):
 #         for j in i :
 #             print(j)
+
+akuna = []
+
+while True:
+    print("Halo! Selamat Datang Boskue Di Program Jual Beli Tiket Konser")
+    print("Silahkan Pilih Kelanjutannya Boskue")
+    print("1. Daftar Akun")
+    print("2. Login")
+    print("3. Exit")
+    print("_________________________________")
+    
+    pilih = input("Pilih Opsi: ")
+    print(" ")
+    
+    if pilih == "1":
+        print("Silahkan Daftar Akun")
+        username = input("Username :")
+        akuna =  False
+        for akun in akuns:
+            if akun [0] == username :
+                akuna = True
+                break
+        if akuna:
+            print("Username Sudah Terpakai!")
+        else:
+            password = input("Passwword: ")
+            akuns.append([username,password, []])
+            print(f"Akun anda sudah jadi bos : {username}")
+            
